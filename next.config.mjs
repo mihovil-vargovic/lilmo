231779c55058ddfba50dc0196bce63574e932ad6
Mihovil <mihovilvargovic@gmail.com>
2026-03-30 17:33:17 +0200

    Remove next-pwa - was breaking Supabase Headers API

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-export default withPWA(nextConfig)
+export default nextConfig
